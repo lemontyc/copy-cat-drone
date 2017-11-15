2641843bbdd84f5faec24e7139b88ee812e522d6
Luis Montoya <montoyacle@hotmail.com>
2017-11-15 12:38:27 -0600

    Debugging x and y controllers

--- scripts/pid_publisher.py
+++ scripts/pid_publisher.py
@@ -7,52 +7,96 @@ from geometry_msgs.msg import Twist
 
 
 def controller_cb(data):
-    global controller_pos_Y
-    controller_pos_Y = data.pose.position.y
+    global position_controller
+    position_controller = data
     
 def drone_cb(data):
-    global drone_pos_Y
-    drone_pos_Y = data.pose.position.y
+    global position_drone
+    position_drone = data
 
-def altitude_pid_cp(data):
-    global drone_y_vel
-    drone_y_vel = data.data
-    #print(type(drone_y_vel))
+def x_pid_cp(data):
+    global drone_cmd_vel
+    drone_cmd_vel.linear.x = data.data
+
+def y_pid_cp(data):
+    global drone_cmd_vel
+    drone_cmd_vel.linear.y = data.data
+
+def z_pid_cp(data):
+    global drone_cmd_vel
+    drone_cmd_vel.linear.z = data.data
 
 # Nodo 
-def publish_altitude():
-    global controller_pos_Y
-    global drone_pos_Y
-    global msg
-    global drone_y_vel
-    
-    drone_y_vel = float()
-    controller_pos_Y = float()
-    drone_pos_Y = float()
-    msg = Twist()
-    
-    # Publishers
-    publish_state = rospy.Publisher("state", Float64, queue_size=10 )
-    publish_setpoint = rospy.Publisher("setpoint", Float64, queue_size=10 )
-    pub_vel = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+def pid_publisher():
+    global position_controller
+    global position_drone
+    global drone_cmd_vel
 
-    rospy.init_node('publish_altitude', anonymous=True)
+    position_controller = PoseStamped()
+    position_drone = PoseStamped()
+    drone_cmd_vel = Twist()
+    
+    # Initialize node
+    rospy.init_node('pid_publisher', anonymous=True)
+    # Publishing frecuency 
     rate = rospy.Rate(50) # 50hz
-    rospy.Subscriber("vrpn_client_node/Quad1/pose", PoseStamped, controller_cb)
-    rospy.Subscriber("vrpn_client_node/Quad2/pose", PoseStamped, drone_cb)
-    rospy.Subscriber("control_effort", Float64, altitude_pid_cp)
 
+    # Publishers ----------------------------------------------------------------
+
+    # Drone X axis PID publisher
+    pub_pid_state_x = rospy.Publisher("drone_x/state", Float64, queue_size=10 )
+    pub_pid_setpoint_x = rospy.Publisher("drone_x/setpoint", Float64, queue_size=10 )
     
+    # Drone Y axis PID publisher
+    pub_pid_state_y = rospy.Publisher("drone_y/state", Float64, queue_size=10 )
+    pub_pid_setpoint_y = rospy.Publisher("drone_y/setpoint", Float64, queue_size=10 )
+    
+    # Drone Z axis PID publisher
+    pub_pid_state_z = rospy.Publisher("drone_z/state", Float64, queue_size=10 )
+    pub_pid_setpoint_z = rospy.Publisher("drone_z/setpoint", Float64, queue_size=10 )
+
+    # Drone cmd_vel publisher
+    pub_cmd_vel = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+
+    # Subscribers ----------------------------------------------------------------
+
+    # Drone X axis PID subscriber
+    rospy.Subscriber("drone_x/control_effort", Float64, x_pid_cp)
+
+    # Drone Y axis PID subscriber
+    rospy.Subscriber("drone_y/control_effort", Float64, y_pid_cp)
+
+    # Drone Z axis PID subscriber
+    rospy.Subscriber("drone_z/control_effort", Float64, z_pid_cp)
+
+    # Drone controller subscriber
+    rospy.Subscriber("vrpn_client_node/Quad1/pose", PoseStamped, controller_cb)
+    
+    # Controlled drone subscriber
+    rospy.Subscriber("vrpn_client_node/Quad2/pose", PoseStamped, drone_cb)
     
     
     while not rospy.is_shutdown():
-        publish_state.publish(drone_pos_Y)
-        publish_setpoint.publish(controller_pos_Y)
-        msg.linear.z = drone_y_vel
-        pub_vel.publish(msg)
+
+        '''
+        # Drone's X axis PID
+        pub_pid_state_x.publish(position_drone.pose.position.z)
+        pub_pid_setpoint_x.publish(position_controller.pose.position.z)
+        '''
+        # Drone's Y axis PID
+        pub_pid_state_y.publish(float(position_drone.pose.position.x - 0.9))
+        pub_pid_setpoint_y.publish(position_controller.pose.position.x )
+        '''
+        # Drone's Z axis PID
+        pub_pid_state_z.publish(position_drone.pose.position.y)
+        pub_pid_setpoint_z.publish(position_controller.pose.position.y)
+        '''
+
+        print(drone_cmd_vel)
+        #pub_cmd_vel.publish(drone_cmd_vel)
         rate.sleep()
         
 
 if __name__ == '__main__':
-    publish_altitude()
+    pid_publisher()
     
